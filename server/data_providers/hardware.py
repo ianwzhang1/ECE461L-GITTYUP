06a9cf6d74e058703d1907ac4248a39cdcc00cc6
SourishW <sourishw@gmail.com>
2023-11-10 14:57:06 -0600

    Combined endpoint

--- server/data_providers/hardware.py
+++ server/data_providers/hardware.py
@@ -60,6 +60,35 @@ class HardwareProvider(DatabaseProvider):
                                            "RETURN h", uuid=hid)[0]  # 0 index unwraps EagerResult
 
         return len(match) > 0
+    
+    def post_details(self, args: list[str], data) -> Response:
+        try:
+            print(data)
+            if data_missing(('hid',), data):
+                return Response('Missing hid.', 400)
+            
+            has_desc = not data_missing(('desc',), data)
+            has_quant = not data_missing(('quant',), data)
+
+            if not (has_desc or has_quant):
+                return Response("Missing both desc and quant, need at least one.")
+            
+            
+            if not self.__hid_exists(data['hid']):
+                return Response('Hardware Set With That hid does not exist', 400)
+                
+            if has_desc:
+                match = self._driver.execute_query("MATCH (h:Hset {uuid: $uuid})"
+                                                "SET h.desc = $desc ", desc=data['desc'], uuid=data["hid"])[0]
+            if has_quant:
+                match = self._driver.execute_query("MATCH (h:Hset {uuid: $uuid})"
+                                                "SET h.quant = $quant ", quant=data['quant'], uuid=data["hid"])[0]
+            
+            return jsonify(True)
+        
+        except Exception as e:
+            return Response(str(e), 400)
+
 
     def post_desc(self, args: list[str], data) -> Response:
         if data_missing(('hid', 'desc'), data):
@@ -104,3 +133,26 @@ class HardwareProvider(DatabaseProvider):
             return Response(str(e), 400)
 
         return jsonify(match[0].get('h.quant'))
+    
+    def get_details(self, args: list[str], params: dict[str, str]) -> Response:
+        try:
+            if not self.__hid_exists(params['hid']):
+                return Response('Hardware Set With That hid does not exist', 400)
+
+            match = self._driver.execute_query("MATCH (h:Hset {uuid: $uuid})"
+                                               " RETURN h.quant", uuid=params['hid'])[0]
+            quant = match[0].get('h.quant')
+
+            match = self._driver.execute_query("MATCH (h:Hset {uuid: $uuid})"
+                                               " RETURN h.desc", uuid=params['hid'])[0]
+            desc = match[0].get('h.desc')
+
+            returning_data = {
+                'quant': quant,
+                'desc': desc
+            }
+            return jsonify(returning_data)
+
+
+        except Exception as e:
+            return Response(str(e), 400)
