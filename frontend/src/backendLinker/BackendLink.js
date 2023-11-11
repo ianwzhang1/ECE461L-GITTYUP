9e43d8d0b05509d2345317ebe9e516c274f36f89
Ian Zhang <124932929+ianwzhang1@users.noreply.github.com>
2023-11-10 18:24:49 -0600

    Changed EC2 URL and fixed compose errors

--- frontend/src/backendLinker/BackendLink.js
+++ frontend/src/backendLinker/BackendLink.js
@@ -2,7 +2,7 @@ import {useContext, useState} from "react";
 import {UserContext, UseUserContext} from "../App";
 
 //export const central_uri = "http://127.0.0.1:5000/";
- export const central_uri = "http://ec2-13-59-237-43.us-east-2.compute.amazonaws.com:5000/"
+ export const central_uri = "http://ec2-3-139-66-199.us-east-2.compute.amazonaws.com:5000/"
 
 export async function post(path, currentUser, body) {
     if (currentUser !== null) {
